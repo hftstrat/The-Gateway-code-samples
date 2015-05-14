2605940ddb01a9fb257811776c71ba46ca3e1b42
jamesmawm <pew pew pew>
2015-05-14 09:02:27 -0500

    update repo

--- examples/send_limit_order.py
+++
@@ -1,9 +0,0 @@
-from classes.socket_client import SocketClient
-import classes.gateway_interface as Gi
-
-ip, port = "10.211.55.3", 4000  # Connection details
-contract, price, is_buy, qty = "XCME_Eq NQ (M15)", 443350, False, 1  # Order details
-
-socket_client = SocketClient(ip, port)
-gateway = Gi.GatewayInterface(socket_client)
-gateway.send_limit_order(contract, is_buy, price, qty)
